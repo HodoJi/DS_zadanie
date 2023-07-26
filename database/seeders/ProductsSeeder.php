7b20f5738f5be13405518f0518146abc4885d27a
HodoJi <82218304+HodoJi@users.noreply.github.com>
2023-07-26 19:38:53 +0200

    categories & products - wip

---
+++ database/seeders/ProductsSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+
+class ProductsSeeder extends Seeder
+{
+    /**
+     * Run the database seeder.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        $products = [
+            1 => [
+                'name' => 'Fiat Punto',
+                'desc' => 'Malé auto od automobilky Fiat',
+                'cost' => 2400,
+                'category_id' => 1
+            ],
+            2 => [
+                'name' => 'Ford Ranger',
+                'desc' => 'Veľké auto od automobilky Ford',
+                'cost' => 42000,
+                'category_id' => 2
+            ]
+        ];
+
+        foreach ($products as $product_id => $productData)
+        {
+            $category = Product::find($product_id);
+
+            if(!$category)
+            {
+                $newProduct = new Product();
+                $newProduct->id = $product_id;
+                $newProduct->name = $productData['name'];
+                $newProduct->desc = $productData['desc'];
+                $newProduct->cost = $productData['cost'];
+                if (isset($productData['category_id']))
+                {
+                    $newProduct->category_id = $productData['category_id'];
+                }
+                $newProduct->save();
+            }
+        }
+    }
+}
